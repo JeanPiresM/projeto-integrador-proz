b027dc734e7be88a9a0d85cb1e8a9a36912ee5cd
Mauricio <mauriciog4silva@hotmail.com>
2024-10-29 17:11:33 -0300

    Enviando atualizações da Home e novas telas: vinhos, Lojas & contato

---
+++ home/script/script.js
@@ -0,0 +1,26 @@
+
+  // Função para verificar se o pop-up deve ser exibido
+  function checkAgeVerification() {
+      const choice = localStorage.getItem("ageVerified");
+      if (!choice) {
+          document.getElementById("popup").style.display = "block";
+      }
+  }
+
+  // Função para confirmar a idade e salvar a escolha se a flag estiver marcada
+  function confirmAge(isOfAge) {
+      const rememberChoice = document.getElementById("rememberChoice").checked;
+      if (rememberChoice) {
+          localStorage.setItem("ageVerified", isOfAge ? "yes" : "no");
+      }
+      document.getElementById("popup").style.display = "none";
+      
+      if (!isOfAge) {
+          alert("Você deve ter mais de 18 anos para acessar este conteúdo.");
+          // Aqui você pode redirecionar para outra página ou fechar o site.
+          window.location.href = "https://snov.io/blog/wp-content/uploads/2023/01/image2-1.png"; // Redirecione se o usuário não tiver idade suficiente.
+      }
+  }
+
+  // Verifica a idade ao carregar a página
+  window.onload = checkAgeVerification;
---
+++ vinhos/script/script.js
@@ -0,0 +1,10 @@
+
+        $(document).ready(function() {
+        $(".owl-carousel").owlCarousel({
+            items: 3,
+            autoplay: true,
+            loop: true,
+            nav: true,
+            dots: true
+        });
+    });
